a1888eadb06b26f61244dfe251d5066097be2fed
lenablechmann <lenablechmann@gmail.com>
2021-06-02 11:28:41 +0200

    dropdown menu via css and html for speed

--- src/hamburgerMenu.js
+++ src/hamburgerMenu.js
@@ -1,3 +1,15 @@
 export default function createHamburgerMenu() {
     console.log("im creating a hamburger menu");
+    const headerTag = document.getElementById('header-tag');
+    const hamburgerHTML = `
+        <section class="hamburger-dropdown">
+            <ul class="project-list-dropdown">
+                <li class="project-link" id="project-1">Project 1</li>
+                <li class="project-link" id="project-2">Project 2</li>
+                <li class="project-link" id="project-3">Project 3</li>
+                <li class="create-project"> 
+                    New Project
+                </li>
+        </section>
+    `;
 }

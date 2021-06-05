712539d378ca586742d1f3f79cccb6ed3857769b
lenablechmann <lenablechmann@gmail.com>
2021-06-05 19:05:02 +0200

    make sure hamburger menu only appears once

--- src/hamburgerMenu.js
+++ src/hamburgerMenu.js
@@ -1,16 +1,20 @@
 export default function createHamburgerMenu() {
-    console.log("im creating a hamburger menu");
     const topNav = document.getElementsByClassName("top-nav");
-    const hamburgerHTML = `
-        <section class="hamburger-dropdown">
-            <ul class="project-list-dropdown">
-                <li class="project-link" id="project-1">Project 1</li>
-                <li class="project-link" id="project-2">Project 2</li>
-                <li class="project-link" id="project-3">Project 3</li>
-                <li class="create-project"> 
-                    New Project
-                </li>
-        </section>
-    `;
-    topNav[0].insertAdjacentHTML('afterend', hamburgerHTML);
+    const hamburger = document.getElementsByClassName("hamburger-dropdown")
+    console.log(hamburger);
+    if (hamburger.length < 1) {
+        const hamburgerHTML = `
+            <section class="hamburger-dropdown">
+                <ul class="project-list-dropdown">
+                    <li class="project-link" id="project-1">Project 1</li>
+                    <li class="project-link" id="project-2">Project 2</li>
+                    <li class="project-link" id="project-3">Project 3</li>
+                    <li class="create-project"> 
+                        New Project
+                    </li>
+            </section>
+        `;
+        topNav[0].insertAdjacentHTML('afterend', hamburgerHTML);
+    }
+
 }

94db8669e8da47cdd420ef4593fad643143b052f
lenablechmann <lenablechmann@gmail.com>
2021-06-09 16:46:36 +0200

    onclick won't work how i want it to, due to async, need another solution

--- src/index.js
+++ src/index.js
@@ -7,17 +7,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const hamburgerMenu = document.getElementById('hamburger-menu');
     hamburgerMenu.addEventListener('click', function(){
         dropdownMenu.createHamburgerMenu();
+        const hamburger = document.getElementsByClassName("hamburger-dropdown");
     });
-    // remove dropdown if user clicks outside
-    const hamburger = document.getElementsByClassName("hamburger-dropdown")[0];
-    if (hamburger) {
-        console.log("dropdown exists");
-    }
     
-    window.onclick = function(event) {
-        if (!event.target.matches('.hamburger-menu')) {
-            console.log("youve clicked outside.");
-        }
-      }
 });
 

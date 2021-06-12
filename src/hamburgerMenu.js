312a8c4715e8b2b5f224fb013817b921550ff2a6
lenablechmann <lenablechmann@gmail.com>
2021-06-12 20:15:47 +0200

    anker dropdown menu in html, but hide in css

--- src/hamburgerMenu.js
+++ src/hamburgerMenu.js
@@ -1,23 +1,5 @@
 export function createHamburgerMenu() {
-
-    // first create the html for the dropdown menu dynamically (will take json in the end)
-    const topNav = document.getElementsByClassName("top-nav");
-    const hamburger = document.getElementsByClassName("hamburger-dropdown")
-    if (hamburger.length < 1) {
-        const hamburgerHTML = `
-            <section class="hamburger-dropdown">
-                <ul class="project-list-dropdown">
-                    <li class="project-link" id="project-1">Project 1</li>
-                    <li class="project-link" id="project-2">Project 2</li>
-                    <li class="project-link" id="project-3">Project 3</li>
-                    <li class="create-project"> 
-                        New Project
-                    </li>
-            </section>
-        `;
-        topNav[0].insertAdjacentHTML('afterend', hamburgerHTML);
-    }
-
+    console.log("you've clicked the menu")
 }
 
 export function deleteHamburgerMenu() {

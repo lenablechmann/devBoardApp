e7e727e65388b367b6447e39489c70d30a3b2330
lenablechmann <lenablechmann@gmail.com>
2021-06-15 16:31:38 +0200

    plan out the json layout for data storage

--- src/hamburgerMenu.js
+++ src/hamburgerMenu.js
@@ -1,4 +1,40 @@
 export function createHamburgerMenu() {
+    // first create the html for the dropdown menu dynamically (will take json in the end)
+    /* a possible json example for this app
+    {
+  "id": 83,
+  "belongsToUser": "a8934uuo3e6267",
+  "projects": [{
+      "name": "todo app", 
+      "issues": [
+        {
+          "name": "Create wireframe", 
+          "status": "review"
+          "tags": ["blocker", "independent"]
+
+        }, {
+          "name": "Figma design", 
+          "status": "todo"
+          "tags": ["blocker", "dependent"]
+        }
+      ]
+    }, {
+      "issues": [
+        {
+          "name": "Create wireframe", 
+          "status": "review"
+          "tags": ["blocker", "independent"]
+
+        }, {
+          "name": "Figma design", 
+          "status": "todo"
+          "tags": ["blocker", "dependent"]
+        }
+      ]
+        }]
+    }]
+}
+     */
     console.log("you've clicked the menu")
 }
 
